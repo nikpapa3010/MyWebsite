b353797bb706bd520f0464b00919a93ab2677709
Niko <60187035+nikpapa3010@users.noreply.github.com>
2021-01-19 21:58:50 +0100

    first commit

---
+++ Project/App/Views/Erstes Projekt.php
@@ -0,0 +1,81 @@
+<html>
+  <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+     <!-- Background image -->
+      <style>
+       body 
+       {
+        background-image: url('http://digital-photography-school.com/wp-content/uploads/flickr/5661878892_15fba42846_o.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+       }
+
+       .transbox {
+  padding: 30px;
+  background-color: rgba(255, 255, 255, 0.9);
+  /* Opacity can be given to background with rgba*/
+  border: 1px solid black;
+  height: fixed;
+
+}
+      </style>
+
+
+      <title>Projekte</title>
+  </head>
+  <body>
+        <div class="container">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+         </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.php"> Startseite </a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="./Lebenslauf.view.php"> Lebenslauf</a> 
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="./Projekte.view.php"> Projekte</a> 
+                    </li>
+                </ul>
+            </div>
+
+            
+            
+        </div>
+        <div class="container" href=".">
+            <div class="transbox">
+                <h2>
+                 Meine Erstes Projekt 
+                </h2>
+                <br>
+                <p>Mein Erstes Projekt: Spider Destroyer in C++</p>
+                <br>
+                <img src="../../Photos/Screenshot (5).png" style="width: fixed;">
+                <br>
+                <br>
+                <p>Mit hilfe von ein Youtube Kanal namens <a href= "https://www.youtube.com/user/ChiliTomatoNoodle" >ChiliTomatoNoodle </a> habe ich meine Erste Programmiersprache gelernt.</p>
+                <p>Weil mir Spiele so viel Intressiert haben wollte ich auch etwas ähnliches machen.</p>
+                <p>Man würde sagen das C++ nicht so eine anfänger Programmiersprache ist aber ich wusste nicht wie anzufangen,</p>
+                <p>und so habe ich C++ gewählt</p>
+
+            </div>
+        </div>
+  </body>
+</html>
---
+++ Project/App/Views/Projekte.view.php
@@ -0,0 +1,80 @@
+<html>
+  <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+     <!-- Background image -->
+      <style>
+       body 
+       {
+        background-image: url('http://digital-photography-school.com/wp-content/uploads/flickr/5661878892_15fba42846_o.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+       }
+
+       .transbox {
+  padding: 30px;
+  background-color: rgba(255, 255, 255, 0.9);
+  /* Opacity can be given to background with rgba*/
+  border: 1px solid black;
+  height: fixed;
+
+}
+      </style>
+
+
+      <title>Projekte</title>
+  </head>
+  <body>
+        <div class="container">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+         </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.php"> Startseite </a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="./Lebenslauf.view.php"> Lebenslauf</a> 
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="./Projekte.view.php"> Projekte</a> 
+                    </li>
+                </ul>
+            </div>
+
+            
+            
+        </div>
+        <div class="container" href=".">
+            <div class="transbox">
+                <h2>
+                 Meine Projekte 
+                </h2>
+                <br>
+                <p> Hier können Sie meine Projekte sehen.</p>
+                <br>
+                <p>Mein Erstes Projekt: Spider Destroyer in C++</p>
+                <img src="../../Photos/Screenshot (5).png" style="width: 50%;">
+                <br>
+                <p>
+                 <a href="./Erstes Projekt.php">Hier</a> können sie Mehr über dieses Projekt Erfahren
+                </p>
+
+            </div>
+        </div>
+  </body>
+</html>
